53cc9b5186a3d79ec8c87c182aee33b468728c7a
Rachel Bingham <rachelbingham@Rachels-MacBook-Air.local>
2022-01-14 14:52:39 -0700

    trying my best

--- index.js
+++ index.js
@@ -219,7 +219,8 @@ console.log(artists[2]['bio'])
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
 
-
+artists[9]['name'] = 'Vincent Van Gogh'
+console.log(artists[9]['name'])
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -229,8 +230,12 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, id) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i].id === id) {
+      return `the artist at index ${array[i].id} is ${array[i].name}`
+    }
+  }
 }
 
 
@@ -244,9 +249,12 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+/*function get20s(array, name) {
+  for(let i = 0; i < array.length; i++) {
+   const birthYear = [];
+    if(array[i]['years'])
+  }
+} */
 
 
 
@@ -259,8 +267,13 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, index) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i].id === id) {
+      delete array[i].id
+      return '19'
+    }
+  }
 }
 
 
@@ -322,8 +335,14 @@ Use artistByCountry to do the following:
 For example artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(array, nationality) {
+  const sameCountry = [];
+  for(let i = 0; i < array.length; i++) {
+    if(array[i].country === country) {
+      sameCountry.push(array[i].name)
+    }
+  }
+  return sameCountry;
 }
 
 
